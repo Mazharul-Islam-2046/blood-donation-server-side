aa1aae684f32eb9912e5eee4871c1b0e4d8716ec
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2023-11-25 22:14:57 +0600

    JWT API added

--- index.js
+++ index.js
@@ -56,6 +56,25 @@ async function run() {
         }
         next();
       };
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+      // jwt API
+      app.post("/jwt", async (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "1h",
+        });
+        res.send({ token });
+      });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
