ab04895bd07f097882b1a5a5e9e90c048ee7c9cd
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2023-11-25 21:56:05 +0600

    JWT Install

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
@@ -9,8 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ypipc9x.mongodb.net/?retryWrites=true&w=majority;`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,31 +27,16 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-
+    // All Collections
+    const userCollection = client.db("bloodDonation").collection("users");
 
     
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -59,11 +44,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Blood Donation')
-})
+app.get("/", (req, res) => {
+  res.send("Blood Donation");
+});
 
 app.listen(port, () => {
   console.log(`Blood Donation ${port}`);
-})
+});
