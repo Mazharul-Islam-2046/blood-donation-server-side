b516e3f4257a38f9199f823a8e5ed4d17b540588
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2023-11-25 22:29:51 +0600

    Delete api

--- index.js
+++ index.js
@@ -144,6 +144,27 @@ async function run() {
               const result = await userCollection.updateOne(filter, updatedDoc);
               res.send(result);
             })
+      
+      
+      
+      
+          //   Delete USER API
+          app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+          })
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
     
     
     
